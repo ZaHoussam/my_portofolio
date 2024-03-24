f05e931f373a7968e4801848dd95aa050c0b5a84
ZaHoussam <mrlhou62@gmail.com>
2024-03-24 23:32:24 +0100

    Add Some Packages : Pinia / Pug / Sass / Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="../public/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<template lang="pug">
+.home
+  v-btn(color="primary") button
 </template>
 
-<style scoped>
+<script setup></script>
+<style lang="scss" scoped>
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+// Use Pinia Store
+import { createPinia } from "pinia";
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+app.use(vuetify).use(pinia).mount("#app");
